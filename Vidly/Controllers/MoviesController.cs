2bf33ee334ad0b4d1f02082a5fa55ee819d7108e
karthikanbazhagan <karthiksanjay@live.com>
2020-11-19 11:13:31 +0530

    Section4:Exercises.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,11 +1,13 @@
 ﻿namespace Vidly.Controllers
 {
+    using System;
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Linq;
     using System.Web.Mvc;
     using System.Web.UI.WebControls;
     using Vidly.Models;
+    using Vidly.ViewModels;
 
     public class MoviesController : Controller
     {
@@ -29,11 +31,67 @@
             return View(movies);
         }
 
-        public ViewResult Details(int id)
+        public ActionResult Details(int id)
         {
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
+            if (movie == null)
+                return HttpNotFound();
+
             return View(movie);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Title = "New Movie",
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Title = "Edit Movie",
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
---
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Vidly.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using Vidly.Models;
+
+    public class MovieFormViewModel
+    {
+        public string Title { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
