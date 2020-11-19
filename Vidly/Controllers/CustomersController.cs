a522dc1bba84efadb82178acc15794d4761e8b03
karthikanbazhagan <karthiksanjay@live.com>
2020-11-19 18:49:46 +0530

    Section5: Validation summmary

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -26,6 +26,7 @@
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel()
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
