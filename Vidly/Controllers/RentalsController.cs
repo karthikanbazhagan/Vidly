501332ceeffd62ef5c4af7d45d8c25824c3b5222
karthikanbazhagan <karthiksanjay@live.com>
2020-12-09 17:39:24 +0530

    Section10: Added View and Controller for NewRentalForm.

---
+++ Vidly/Controllers/RentalsController.cs
@@ -0,0 +1,13 @@
+﻿namespace Vidly.Controllers
+{
+    using System.Web.Mvc;
+
+    public class RentalsController : Controller
+    {
+        // GET: Rentals
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
